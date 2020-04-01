9fc873642c0cd6d514466321196435434ea29414
JeffRodL <61574052+JeffRodL@users.noreply.github.com>
2020-03-31 18:36:50 -0600

    Update Problema6Practica4D.c

--- Problema6Practica4D.c
+++ Problema6Practica4D.c
@@ -11,15 +11,17 @@ Salida: numero entero
 #include <stdlib.h>
 #include <math.h>
 //numerar los pasos del pseudocodigo
-
+//al igual que con la anterior, se trabajan con funciones recursivas tomando siempre un caso base.
 float suma4(float);
 int main(){
-
+        //Variable que digitará el usuario de hasta dónde quiere su sumatoria.
         float n;
 
         printf("\nIngrese un valor hasta donde quiere su sumatoria:");
         scanf("%f", &n);
 
+	//Puede existir el caso que el usuario intente agregar un número invalido por eso agregamos este ciclo
+	//while que le dirá que debe ingresar un número aceptable.
         if(n <= 1){
                 while( n<=1){
                 printf("\nIngrese un valor aceptado que sea positivo:");
@@ -29,7 +31,7 @@ int main(){
                 }
 
         }else{
-
+        //si el usuario agrega un valor acepdato, la máquina imprimirá el resultado
         printf("\nEl resultado de la sumatoria es: %.2f \n", suma4(n));
         }
 
@@ -37,7 +39,7 @@ int main(){
 
         return 0;
 }
-
+//Función de recursión con el valor inicial para poder determinar los siguientes.
 float suma4(float x){
         if(x == 2){
                 x = 0.70;
