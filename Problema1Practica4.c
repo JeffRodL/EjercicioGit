82ee0e92a7eb414e67b1eb4b53ee613642a3f0d5
JeffRodL <61574052+JeffRodL@users.noreply.github.com>
2020-03-31 18:03:37 -0600

    Update Problema1Practica4.c

--- Problema1Practica4.c
+++ Problema1Practica4.c
@@ -13,21 +13,24 @@ Salida: vectores ordenados de manera ascendente o descendente.
 
 
 int main(){
-
+	
+	//Declaración de Variables a utilizar.
 	int vector[10]={2,4,6,8,10,12,14,16,18,20};
 	int i, j, k;
 	char x;
 
 	printf("\nA continuación se mostrará un programa que ordenará un vector de 10 elementos con números pares del 1-20. Elegir si los quiere ver de forma ascendente (a) o forma descendente (d) \n¿ascendente o descendente?");
 	
-	scanf("%c", &x);
-
+	scanf("%c", &x); //recogemos el valor que ingresa el usuario.
+	
+	//Por si el usuario se pasa de listo y agrega un valor incorrecto.
 	while( x !='a' && x!='d'){
 	
 	printf("\nNo se continuará con el programa hasta que ingrese un valor vvalido. \n¿ascendente (a) o descendente (d)?");
 	scanf("%c",&x);
 	} 
-	
+	//Si el usuario hace lo correcto, el programa seguirá trabajando. Se ordena para forma descendente si el valor
+	//ingresado es a 
 	if(x=='a'){
 		printf("\nForma ascendente:\n");
 			for(i=0; i<=9; i++){
@@ -39,12 +42,13 @@ int main(){
 					vector[j]=x;	
 				}
 			}
-			}
+		}
+		//Y se imprime el resultado dentro de un for, para que muestre para cada vector.
 		for(i=0; i<=9; i++){
 			printf("%i\n", vector[i]);
 		}
 	}
-
+		//Lo mismo si el valor ingresado es una d, solamente que aquí se mostrará en forma descendente.
 	if(x == 'd'){
 		printf("\nForma descendente:\n");
 		for(i=0; i<9; i++){
@@ -60,7 +64,7 @@ int main(){
 			}
 		}
 	}
-
+	//así mismo se vuelve a imprimir dentro de un for para dar un valor a cada vector.
 	for(i=0; i<=9; i++){
 		printf("\n%i",vector[i]);
 	}
